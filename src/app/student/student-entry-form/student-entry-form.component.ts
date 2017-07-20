1c9f6dd6a925b23df0bcf47ba29241c18a715b36
Gregory Dodd <ikkyixo@gmail.com>
2017-07-20 18:21:38 -0500

    Updated component with logic to create the student form and add the student upon submission

--- src/app/student/student-entry-form/student-entry-form.component.ts
+++ src/app/student/student-entry-form/student-entry-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StudentProviderService } from '../student-provider/student-provider.service';
 
 @Component({
   selector: 'app-student-entry-form',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-entry-form.component.scss']
 })
 export class StudentEntryFormComponent implements OnInit {
+  studentEntryForm: FormGroup;
 
-  constructor() { }
+
+  constructor(
+    public studentService: StudentProviderService,
+    public formBuilder: FormBuilder
+  ) {
+    this.createStudentEntryForm();
+  }
 
   ngOnInit() {
+  }
+
+  public createStudentEntryForm() {
+    this.studentEntryForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      dob: ['', Validators.required],
+      gpa: ['', Validators.required]
+    });
+  }
+
+  public addStudent(student) {
+    this.studentService.addStudent(student);
+  }
+
+  public onSubmit() {
+    const student = this.prepareStudent(this.studentEntryForm.value);
+    this.addStudent(student);
+    this.createStudentEntryForm();
+  }
+
+  public prepareStudent(formData) {
+    const studentData = {
+      id: Math.floor(Math.random() * 99) + 1,
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      dob: formData.dob,
+      gpa: formData.gpa
+    };
+
+    return studentData;
   }
 
 }

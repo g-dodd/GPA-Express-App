f649161546d1a989e1253ee0cfb0f90898ec6de1
Gregory Dodd <ikkyixo@gmail.com>
2017-07-20 18:20:19 -0500

    Added data store for students and updated addStudent function to push student to data store and Subject

--- src/app/student/student-provider/student-provider.service.ts
+++ src/app/student/student-provider/student-provider.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs/Rx';
+import { Student } from './student';
 
 @Injectable()
 export class StudentProviderService {
-
-  private students: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  private studentData: Student[] = [];
+  private students: BehaviorSubject<Array<Student>> = new BehaviorSubject(this.studentData);
 
   constructor() { }
 
-  public getAllStudents() {
+  public getAllStudents(): Observable<Student[]> {
     return this.students.asObservable();
   }
 
   public addStudent(student) {
-    
+    this.studentData.push(student);
+    this.students.next(this.studentData);
   }
 }

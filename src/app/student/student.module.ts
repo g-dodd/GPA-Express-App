a1b362075a37bc94351c3a74ed0fe472c49284aa
Gregory Dodd <ikkyixo@gmail.com>
2017-07-20 18:22:40 -0500

    Added dependency for Reactive Forms Module to student module

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StudentEntryFormComponent } from './student-entry-form/student-entry-form.component';
 import { StudentDatatableComponent } from './student-datatable/student-datatable.component';
 import { StudentProviderService } from './student-provider/student-provider.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
     StudentEntryFormComponent,

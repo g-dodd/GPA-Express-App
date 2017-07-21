8d9417e34633decb4eaef7d120b8f065812e1ed5
Gregory Dodd <ikkyixo@gmail.com>
2017-07-21 01:31:25 -0500

    Updated component with subscription to alerts and a function to close the alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertProviderService } from './alert-provider/alert-provider.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  alert = '';
+
+  constructor(
+    public alertService: AlertProviderService
+  ) {}
+
+  ngOnInit() {
+    this.subscribeToAlerts();
+  }
+
+  public subscribeToAlerts() {
+    this.alertService.alert.subscribe(
+      response => {
+        this.alert = response;
+      }
+    );
+  }
+
+  public closeAlert() {
+    this.alertService.clearAlert();
+  }
+
 }

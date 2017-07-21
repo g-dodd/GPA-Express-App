78e63ad28b77a9f1e5ddba1a1c7d2484eb78e584
Gregory Dodd <ikkyixo@gmail.com>
2017-07-20 19:29:56 -0500

    Added api url and http dependencies. Updated getAllStudents function to retrieve response from api server

--- src/app/student/student-provider/student-provider.service.ts
+++ src/app/student/student-provider/student-provider.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { BehaviorSubject, Observable } from 'rxjs/Rx';
 import { Student } from './student';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StudentProviderService {
   private studentData: Student[] = [];
   private students: BehaviorSubject<Array<Student>> = new BehaviorSubject(this.studentData);
+  private apiUrl = 'http://gpa-express-api.gregdodd.com/';
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
 
-  public getAllStudents(): Observable<Student[]> {
-    return this.students.asObservable();
+  public getAllStudents(): Observable<any> {
+
+    return this.http.get(this.apiUrl + 'students')
+      .map((res: Response) => res.json());
   }
 
   public addStudent(student) {

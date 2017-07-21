ebe6a43931dd82a77187573459ef3411313422b0
Gregory Dodd <ikkyixo@gmail.com>
2017-07-21 01:31:41 -0500

    Added alert service as dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
+import { AlertProviderService } from './alert-provider/alert-provider.service';
 import { StudentModule } from './student/student.module';
 import { StudentProviderService } from './student/student-provider/student-provider.service';
 
@@ -15,7 +16,7 @@ import { StudentProviderService } from './student/student-provider/student-provi
     HttpModule,
     StudentModule
   ],
-  providers: [StudentProviderService],
+  providers: [AlertProviderService, StudentProviderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

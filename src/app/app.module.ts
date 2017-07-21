da701361921f17030128f7b2f4e1d3fc36bd3fe0
Gregory Dodd <ikkyixo@gmail.com>
2017-07-20 19:28:07 -0500

    Added HttpModule and StudentProviderService dependencies to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 import { StudentModule } from './student/student.module';
+import { StudentProviderService } from './student/student-provider/student-provider.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { StudentModule } from './student/student.module';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     StudentModule
   ],
-  providers: [],
+  providers: [StudentProviderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
